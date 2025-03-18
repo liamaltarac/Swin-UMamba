85a2b82b72960b7616ef769ff4b3a05d4fa325b1
Your Name <liamaltarac@gmail.com>
2025-03-17 18:32:10 -0700

    modified l2 loss

--- swin_umamba/nnunetv2/training/loss/l2_loss.py
+++ swin_umamba/nnunetv2/training/loss/l2_loss.py
@@ -5,7 +5,12 @@ import numpy as np
 #Source : https://github.com/Phyrise/nnUNet_translation/blob/b07d03cd456762455877a2c0005fba0eb7fec982/nnunetv2/training/loss/mse.py
 class L2(nn.MSELoss):
     def forward(self, input: Tensor, target: Tensor) -> Tensor:
-
+        mse_loss = nn.MSELoss(reduction='none')
+        loss = mse_loss(input, target)
+        mask = np.ones_like(target)[target < 0.1]
+        loss = (loss * mask.float()).sum() 
+        non_zero_elements = mask.sum()
+        mse_loss_val = loss / non_zero_elements
         #print("TARGET", len(target))
-        print(input.shape , target.shape)
-        return super().forward(input[target > 0.5], target[target > 0.5]) 
+        print(input.shape , target.shape, loss.shape)
+        return mse_loss_val  #super().forward(input[target > 0.5], target[target > 0.5]) 
